401cc630d4f012d5910cebcc662c0d77b877a693
osvifl1516daw2 <osvifl1516daw2@gmail.com>
2015-11-07 16:57:47 +0100

    Creant un primer conflicte amb la branca01

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -28,5 +28,6 @@ public class Prjava02 {
         }
         System.out.println("Creació d'una branca del projecte prjava02"); 
         System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+        System.out.println("Conflicte típic: Codi diferent a una mateixa línia");
     }
 }
