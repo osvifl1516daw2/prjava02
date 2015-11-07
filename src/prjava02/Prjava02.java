cc5ffe422f24d7bc8fc52b72ce1f43f93b59b3fa
osvifl1516daw2 <osvifl1516daw2@gmail.com>
2015-11-07 16:18:40 +0100

    Treballant amb ordres Git i labranca01 v0.1

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -28,5 +28,6 @@ public class Prjava02 {
         }
         System.out.println("Creació d'una branca del projecte prjava02"); 
         System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+        System.out.println("Afegint codi a la branca01 del projecte prjava02");
     }
 }
